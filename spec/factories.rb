05abef67434a93b4ddfbeb3ab98a371d9614a557
Casey Li <casey.li@gmail.com>
2011-06-25 17:26:50 -0400

    User model with passwords, and user show page.

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Casey Li"
+  user.email                  "casey.li@gmail.com"
+  user.password               "password"
+  user.password_confirmation  "password"
+end
