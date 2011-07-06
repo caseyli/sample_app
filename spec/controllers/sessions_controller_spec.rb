eb1c9bd556afde027030f1ba33e8cf8c8d390c57
Casey Li <casey.li@gmail.com>
2011-07-05 21:40:00 -0400

    Changed POST create test to match redirect to root_path

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -52,9 +52,9 @@ describe SessionsController do
         controller.should be_signed_in
       end
       
-      it "should redirect to the user show page" do
+      it "should redirect to the root path" do
         post :create, :session => @attr
-        response.should redirect_to(user_path(@user))
+        response.should redirect_to(root_path)
       end
   end
 
